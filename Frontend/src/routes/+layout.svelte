480ddc3fec162b19529f514445e0feca19dcdb5d
Vetleskrett <vetle.skretting@hotmail.no>
2024-10-11 16:30:14 +0200

    Init frontend

---
+++ Frontend/src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import '../app.css';
+</script>
+
+<div class="fixed flex h-16 w-full flex-row bg-[#1D6F8B] text-white">
+	<a
+		href="/courses"
+		class="flex h-16 w-28 items-center justify-center text-center hover:bg-black hover:bg-opacity-10"
+	>
+		<h2 class="text-lg font-semibold">Courses</h2>
+	</a>
+</div>
+<div class="h-16" />
+
+<slot></slot>
